a1de234152d462394042ef354f75f013d6d482c1
superjoefly <superjoefly@gmail.com>
2018-01-14 01:10:48 -0700

    Removed sessions to maintain old data in form.

--- contact.php
+++ contact.php
@@ -1,55 +1,164 @@
-<?php session_start(); error_reporting(0);?>
 <!DOCTYPE html>
 <html>
-   <head>
-      <meta charset="utf-8">
-      <title>Contact</title>
+<head>
+   <meta charset="utf-8">
+   <title>Contact</title>
+   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+   <link rel="stylesheet" href="styles.css">
+</head>
+<body>
 
-      <style>
-         .error {
-            color: red;
+   <?php
+      // define variables and set to empty values
+      $name_err = $email_err = $website_err = $gender_err = "";
+      $name = $email = $gender = $comment = $website = "";
+
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          if (empty($_POST["name"])) {
+              $name_err = "Name is required";
+          } else {
+              $name = test_input($_POST["name"]);
+         // check if name only contains letters and whitespace
+         if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+             $name_err = "Only letters and white space allowed";
          }
-      </style>
-   </head>
-   <body>
-      <h2>PHP Form Validation Example</h2>
-      <p><span class="error">* required field.</span></p>
+          }
 
-      <form method="post" action="confirmation.php">
+          if (empty($_POST["email"])) {
+              $email_err = "Email is required";
+          } else {
+              $email = test_input($_POST["email"]);
+         // check if e-mail address is well-formed
+         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+             $email_err = "Invalid email format";
+         }
+          }
 
-        Name: <input type="text" name="name" value="<?php echo $name;?>" required>
-        <span class="error">* <?php echo $_SESSION['name'];?></span>
-        <br><br>
+          if (empty($_POST["website"])) {
+              $website = "";
+          } else {
+              $website = test_input($_POST["website"]);
+         // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+         if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
+             $website_err = "Invalid URL";
+         }
+          }
 
-        E-mail: <input type="text" name="email" value="<?php echo $email;?>" required>
-        <span class="error">* <?php echo $_SESSION['email'];?></span>
-        <br><br>
+          if (empty($_POST["comment"])) {
+              $comment = "";
+          } else {
+              $comment = test_input($_POST["comment"]);
+          }
 
-        Website: <input type="text" name="website" value="<?php echo $website;?>">
-        <span class="error"><?php echo $_SESSION['website'];?></span>
-        <br><br>
+          if (empty($_POST["gender"])) {
+              $gender_err = "Gender is required";
+          } else {
+              $gender = test_input($_POST["gender"]);
+          }
+      }
 
-        Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-        <br><br>
+      function test_input($data)
+      {
+          $data = trim($data);
+          $data = stripslashes($data);
+          $data = htmlspecialchars($data);
+          return $data;
+      }
+   ?>
 
-        Gender: <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") {
-    echo "checked";
-}?> value="female" required>Female
-        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") {
-    echo "checked";
-}?> value="male">Male
-        <span class="error">* <?php echo $_SESSION['gender']?></span>
-        <br><br>
+   <!-- Form to Validate -->
+   <div class="w3-container form-container" id="form">
+      <div class="w3-card-4">
+         <div class="w3-container w3-teal">
+            <h2 class="w3-center">Form with Validation</h2>
+         </div>
+            <p class="w3-padding">
+               <span class="error">* Required Field.</span>
+            </p>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" class="w3-container">
 
-        <input type="submit" name="submit" value="Submit">
-      </form>
+              <p>Name:
+                 <span class="error">* <?php echo $name_err;?></span>
+              </p>
+              <input type="text" name="name" value="<?php echo $name;?>" class="w3-input" required autofocus>
 
+              <p>Email:
+                 <span class="error">* <?php echo $email_err;?></span>
+              </p>
+              <input type="text" name="email" value="<?php echo $email;?>" class="w3-input" required>
+
+              <p>Website:
+                  <span class="error"><?php echo $website_err;?></span>
+              </p>
+              <input type="text" name="website" value="<?php echo $website;?>" class="w3-input">
+
+              <p>Comment:</p>
+              <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+              <br>
+
+               <div class="gender-box">
+                  <p>Gender:
+                     <span class="error">* <?php echo $gender_err;?></span>
+                  </p>
+                  <input type="radio" name="gender"
+                  <?php
+                     if (isset($gender) && $gender=="female") {
+                         echo "checked";
+                     }
+                  ?> value="female" class="w3-radio" required>
+                  <label>Female</label>
+                     <br>
+                  <input type="radio" name="gender"
+                  <?php
+                     if (isset($gender) && $gender=="male") {
+                         echo "checked";
+                     }
+                  ?> value="male" class="w3-radio">
+                  <label>Male</label>
+               </div>
+
+              <input type="submit" name="submit" value="Submit" class="w3-margin w3-button w3-right w3-teal" onsubmit="return renderData()">
+
+            </form>
+
+            <div class="w3-container w3-teal">
+               <p class="w3-center">NewUp Developments</p>
+            </div>
+      </div>
+   </div>
+
+
+   <div class="w3-container w3-hide" id="data">
+      <!-- Store in database and/or display input to user -->
       <?php
-          unset($_SESSION['name']);
-          unset($_SESSION['email']);
-          unset($_SESSION['website']);
-          unset($_SESSION['gender']);
+        echo "<h2>Your Input:</h2>";
+        echo $name;
+        echo "<br>";
+        echo $email;
+        echo "<br>";
+        echo $website;
+        echo "<br>";
+        echo $comment;
+        echo "<br>";
+        echo $gender;
       ?>
+      <p>Thank you for contacting us!</p>
+      <a href="index.php">Home</a>
+   </div>
 
-   </body>
+
+   <script>
+      window.onload = function(e) {
+         console.log('Window Onload')
+      }
+      function renderData() {
+         // let data = document.getElementById('data');
+         // let form = document.getElementById('form');
+         //
+         // data.classList.remove('w3-hide')
+         console.log('Hello')
+      }
+   </script>
+
+</body>
 </html>
--- index.php
+++ index.php
@@ -3,11 +3,22 @@
   <head>
     <meta charset="utf-8">
     <title>Home</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="styles.css">
   </head>
   <body>
-    <h1>Home page</h1>
 
-    <a href="contact.php">Contact</a>
+    <header>
+      <h1>Home Page</h1>
+    </header>
+
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </nav>
 
   </body>
 </html>
